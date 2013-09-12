244eb92f6a6e6e3e5a9b3ae6edff91e6178a0286
Jay Kanakiya <kanakiyajay@yahoo.com>
2013-09-12 12:41:07 +0530

    CSS slider binding

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*!
-** Akshar
+** HTML Email
 ** Licensed under the Apache License v2.0
 ** http://www.apache.org/licenses/LICENSE-2.0
 ** Built by Jay Kanakiya ( @techiejayk )
@@ -7,7 +7,31 @@
 
 var App = angular.module('akshar',[]);
 
-App.controller('mainCtrl',function  ($scope) {
-  $scope.message = "Hello World" ;
-})
+App.factory('Model',function  () {
+  return [
+    { text : "Jay Kanakiya" , font_size : 40 , letter_spacing : 1 }
+  ]
+});
 
+App.controller('mainCtrl',function  ($scope,Model) {
+  $scope.model = Model ;
+  $scope.current = { text : ""  , font_size : 1 } ;
+
+  $scope.toToolbar = function  (r) {
+    $scope.current = $scope.model[r] ;
+  }
+
+  $scope.getStyle = function  (index) {
+    var cssStyles = "" ;
+    var cssObj = $scope.model[index] ;
+    var stylesList = [ "font_size" , "letter_spacing" ] ;
+    stylesList.forEach(function  (i) {
+      cssStyles += i.replace("_","-") + ":" + cssObj[i] + ";" ;
+    });
+    return cssStyles ;
+  }
+
+  $scope.init = function  () {
+    $scope.current = $scope.model[0];
+  }
+});
