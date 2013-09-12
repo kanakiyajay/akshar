bb97b0021e77a3bd5bda1547d67320f786c739f6
Jay Kanakiya <kanakiyajay@yahoo.com>
2013-09-12 13:24:53 +0530

    Spectrum js Color-picker

---
+++ README.md
@@ -0,0 +1,2 @@
+* http://jsfiddle.net/bmleite/2Pk3M/2/
+* http://bgrins.github.io/spectrum/
--- js/app.js
+++ js/app.js
@@ -9,13 +9,13 @@ var App = angular.module('akshar',[]);
 
 App.factory('Model',function  () {
   return [
-    { text : "Jay Kanakiya" , font_size : 40 , letter_spacing : 1 }
+    { text : "Jay Kanakiya" , font_size : 40 , letter_spacing : 1 , color : "#000" , background_color : "#fff" }
   ]
 });
 
 App.controller('mainCtrl',function  ($scope,Model) {
   $scope.model = Model ;
-  $scope.current = { text : ""  , font_size : 1 } ;
+  $scope.current = {} ;
 
   $scope.toToolbar = function  (r) {
     $scope.current = $scope.model[r] ;
@@ -24,7 +24,7 @@ App.controller('mainCtrl',function  ($scope,Model) {
   $scope.getStyle = function  (index) {
     var cssStyles = "" ;
     var cssObj = $scope.model[index] ;
-    var stylesList = [ "font_size" , "letter_spacing" ] ;
+    var stylesList = [ "font_size" , "letter_spacing" , "color" , "background_color"] ;
     stylesList.forEach(function  (i) {
       cssStyles += i.replace("_","-") + ":" + cssObj[i] + ";" ;
     });
@@ -34,4 +34,34 @@ App.controller('mainCtrl',function  ($scope,Model) {
   $scope.init = function  () {
     $scope.current = $scope.model[0];
   }
+});
+
+/* Directives */
+App.directive('uiColorpicker', function() {
+    return {
+        restrict: 'E',
+        require: 'ngModel',
+        scope: false,
+        replace: true,
+        template: "<span><input class='input-small' /></span>",
+        link: function(scope, element, attrs, ngModel) {
+            var input = element.find('input');
+            var options = angular.extend({
+                color: ngModel.$viewValue,
+                clickoutFiresChange : true ,
+                showInput : true ,
+                change: function(color) {
+                    scope.$apply(function() {
+                      ngModel.$setViewValue(color.toHexString());
+                    });
+                }
+            }, scope.$eval(attrs.options));
+
+            ngModel.$render = function() {
+              input.spectrum('set', ngModel.$viewValue || '');
+            };
+
+            input.spectrum(options);
+        }
+    };
 });
